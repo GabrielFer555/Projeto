43cdd2f07ee93f5c4c4d347954c68c826f32a473
Gabriel Fernandes <kaiberimanos3@gmail.com>
2023-05-07 21:05:53 -0300

    APÍ em JSON está recebendo um único argumento

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
 app.use(express.json());
 
 app.use(cors());
@@ -9,60 +11,55 @@ app.get('/', async (req, res, next) => {
 });
 
 
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-app.post("/checklogin", async(req, res) => {
+app.post("/checklogin", async (req, res) => {
     console.log(req.body);
     const data = await prisma.users.findFirst({
-        where:{
-            nome:req.body.nome,
+        where: {
+            nome: req.body.nome,
             password: req.body.password
         }
     })
-    if(!data){
+    if (!data) {
         console.log('no');
-        return res.status(404).json({message:"Erro: Not Found"})
+        return res.status(404).json({ message: "Erro: Not Found" })
     }
-    else{
+    else {
         console.log('yes')
-        return res.status(200).json({message:"Logado!"});
+        return res.status(200).json({ message: "Logado!" });
     }
 })
-
-app.post("/createaccount", async(req, res) => {
-    console.log(req.body);
-    const nome = req.body.nome;
-    const password = req.body.password;
-    const data = await prisma.users.findFirst({
-        where:{
-            nome:req.body.nome,
-            password: req.body.password
+app.post('/checkregister', async (req, res) => {
+    const user = req.body.user;
+    const verification = await prisma.users.findFirst({
+        where: {
+            nome: user
         }
     })
-    if(data){
-        return res.status(200).json({message:"Usuário já existe"})
-    }else{
-        console.log(req.body);
-        try{
-            const creation = await prisma.users.create({
-                data:{
-                    nome:nome, 
-                    password: password
-                }
-            })
-            return res.status(400).json({message:"Usuário criado"})
-        }
-        catch (error){
-            console.log(error)
-            return res.status(404).json({message: "Algo deu errado"})
-        }
+    if (verification) {
+        return res.status(200).json({ message: "Usuário já existe!" });
+    } else {
+        return res.status(404).json({ message: "Usuário não encontrado!" });
     }
 })
 
+app.post('/createaccount',
+    async (req, res) => {
+        console.log(req.body.password)
+        const { nome, password } = req.body;
+        const user = await prisma.users.create({
+            data: {
+                nome: req.body.nome,
+                password: req.body.password
+            }
+        });
+        return res.status(201).json({ message: 'Usuário criado com sucesso!' });
 
-
+    }
+);
 
 app.listen(8080, function () {
     console.log('listening on port 8080');
-    
+
 })
